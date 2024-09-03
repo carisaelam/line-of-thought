ad11073e74f2de82e92e1f947e0037d274fd3d08
carisaelam <carisaelam@gmail.com>
2024-09-03 14:06:52 -0400

    Remove title from posts

--- spec/features/create_post_spec.rb
+++ spec/features/create_post_spec.rb
@@ -5,11 +5,10 @@ RSpec.describe 'Creating a post', type: :feature do
     user = FactoryBot.create(:user)
     sign_in_as(user)
     visit new_post_path
-    fill_in 'Title', with: 'My first post'
     fill_in 'Body', with: 'This is the best first post ever'
     click_on 'Create Post'
 
 
-    expect(page).to have_content('My first post')
+    expect(page).to have_content('This is the best first post ever')
   end
 end
