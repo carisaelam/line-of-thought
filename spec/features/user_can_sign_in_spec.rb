bc0c9dc49c3b5e7fb456a49fe46aa59e0d8074ab
carisaelam <carisaelam@gmail.com>
2024-09-04 09:08:57 -0400

    Switch order of username and body on posts

--- spec/features/user_can_sign_in_spec.rb
+++ spec/features/user_can_sign_in_spec.rb
@@ -10,3 +10,22 @@ RSpec.describe 'User Sign-In', type: :feature do
     expect(page).to have_content('Signed in successfully.')
   end
 end
+require 'rails_helper'
+
+RSpec.describe 'Can identify user posts' do
+  user1 = FactoryBot.create(:user)
+  user2 = FactoryBot.create(:user)
+  first_post = FactoryBot.create(:post, user: user1)
+
+  scenario 'User has many posts' do
+    expect(user1.posts).to include(first_post)
+  end
+
+  scenario 'User has no posts' do
+    expect(user2.posts).to be_empty
+  end
+
+  scenario 'Post has one user' do
+    expect(first_post.user).to be(user1)
+  end
+end
