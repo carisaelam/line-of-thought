c1ef3ef356303af764b92beb6fbdabbdd995d508
carisaelam <carisaelam@gmail.com>
2024-09-08 12:29:24 -0400

    Profile page displays followers

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    email { Faker::Internet.email }
-    full_name { Faker::Internet.name }
+    email { Faker::Internet.unique.email }
+    full_name { Faker::Name.unique.name }
     password { Faker::Internet.password(min_length: 8) }
   end
 end
---
+++ spec/features/can_display_followees_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'Users can follow other users' do
+  Faker::UniqueGenerator.clear
+  user1 = FactoryBot.create(:user)
+  user2 = FactoryBot.create(:user)
+
+  before do
+    user1.followees << user2
+  end
+
+  scenario 'User1 is following User2' do
+      sign_in_as(user1)
+      visit user_path(user1)
+
+      puts "user1 email: #{user1.email}"
+
+      puts "user1 full_name: #{user1.full_name}"
+      puts "user2 full_name: #{user2.full_name}"
+
+      expect(page).to have_content(user1.full_name)
+      expect(page).to have_content(user2.full_name)
+  end
+end
