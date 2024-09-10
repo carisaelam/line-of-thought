f9c9a0e2f9ed99719ef021388776e6c3f482b361
carisaelam <carisaelam@gmail.com>
2024-09-10 12:14:20 -0400

    Update capybara tests to work with turbo

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,14 +1,32 @@
 class FollowsController < ApplicationController
+  include ActionView::RecordIdentifier
+
   def create
     @user = User.find(params[:user_id])
     current_user.follow(@user)
-    redirect_to @user
+
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: [
+          turbo_stream.replace(dom_id(@user, :follow_buttons), partial: "users/follow_buttons", locals: { user: @user }),
+          turbo_stream.replace(dom_id(@user, :follow_counts), partial: "users/follow_counts", locals: { user: @user })
+        ]
+      end
+    end
   end
 
   def destroy
     @user = User.find(params[:user_id])
     current_user.unfollow(@user)
-    redirect_to @user
+
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: [
+          turbo_stream.replace(dom_id(@user, :follow_buttons), partial: "users/follow_buttons", locals: { user: @user }),
+          turbo_stream.replace(dom_id(@user, :follow_counts), partial: "users/follow_counts", locals: { user: @user })
+        ]
+      end
+    end
   end
 
   def followers
--- spec/features/can_follow_and_unfollow_spec.rb
+++ spec/features/can_follow_and_unfollow_spec.rb
@@ -3,6 +3,11 @@ require 'rails_helper'
 RSpec.describe 'Following' do
   user1 = FactoryBot.create(:user)
   user2 = FactoryBot.create(:user)
+  user3 = FactoryBot.create(:user)
+  user4 = FactoryBot.create(:user)
+  before do
+    user3.follow(user4)
+  end
 
   scenario 'User can follow another user' do
       sign_in_as(user1)
@@ -14,12 +19,12 @@ RSpec.describe 'Following' do
   end
 
   scenario 'User can unfollow another user' do
-      sign_in_as(user1)
-      visit user_path(user2)
-      click_on 'Follow' # to trigger a follow event and change the button
+      sign_in_as(user3)
+      visit user_path(user4)
+
       click_on 'Unfollow'
 
-      expect(user2.followers).not_to include(user1)
+      expect(user4.followers).not_to include(user3)
   end
 
   scenario 'User cannot follow themselves' do
